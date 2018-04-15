8ae48f06b070e1dc81062d8d2269ed0b19be41a6
MONstruck <volodya.poberez@gmail.com>
2018-04-15 13:44:15 +0300

    second
    Signed-off-by: MONstruck <volodya.poberez@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForCourse
+{
+    class Program : ToDo
+    {
+ 
+            static void Main(string[] args)
+            {
+                double a, b, c, d, r;         // input side of the cube and window          
+                Console.WriteLine("Please write a 1 side of the cube");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please write a 2 side of the cube");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please write a 3 side of the cube");
+                c = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please write a 1 side of the window");
+                d = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please write a 2 side of the cube");
+                r = double.Parse(Console.ReadLine());
+                if (IsNotNegative(a, b, c, d, r))// realization
+                {
+                    if ((Check(a, d, r) && Check(b, d, r)) || (Check(b, d, r) && Check(c, d, r)) || (Check(a, d, r) && Check(a, d, r)))
+                    {
+
+                        Console.WriteLine("Cube fit in");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cube not fit");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You write wrong parametrs");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
---
+++ ToDo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForCourse
+{
+   public class ToDo
+    {
+        //function checks whether the cube will enter
+       protected static bool Check(double a1, double d1, double r1)
+        {
+
+            if (a1 < d1 || a1 < r1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }
+        //function checks the input data for negative and zero values
+      protected static bool IsNotNegative(double a1, double d1, double r1, double c1, double b1)
+        {
+
+            if (a1 <= 0 || d1 <= 0 || c1 <= 0 || b1 <= 0 || r1 <= 0)
+            {
+
+                return false;
+            }
+            else { return true; }
+        }
+    }
+}
